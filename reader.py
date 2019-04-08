6331a1591016c21a1d60befe91e101ac42124907
Chris Druta <cndruta@email.arizona.edu>
2019-04-07 22:38:53 -0700

    Added reader that loops through weeks for data

---
+++ reader.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+def read_csv(filename):
+    """
+    Opens and parses/processes csv files to return data and labels
+
+    Args:
+        filename - String containing a csv file's directory
+
+    Returns:
+        X - Data to be used as input features for the neural net
+        y - The labels for each data point
+    """
+    data = pd.read_csv(filename, header=0).values
+
+    weeklyData = []
+    lastWeekDate = datetime.strptime(data[0, 0], "%Y-%m-%d")
+    
+    # row: date,open,high,low,close,volume,unadjustedVolume,change,changePercent,vwap,label,changeOverTime
+    for row in data[1:]:
+        currentDate = datetime.strptime(row[0], "%Y-%m-%d")
+        
+        if (currentDate - lastWeekDate).days >= 7:
+            weeklyData.append(row)
+            lastWeekDate = currentDate
+
+    # TODO: Parse/calculate label, probably inside loop above
+    y = np.zeros(len(weeklyData))
+
+    return np.array(weeklyData), y
+
+print(read_csv('./data/AAPL.csv')[0])
