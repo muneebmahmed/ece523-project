88d4644d0746105e846693f11a7396f7288d306c
Muneeb Ahmed <muneebmahmed@gmail.com>
2019-05-02 15:08:20 -0700

    functions for updown

---
+++ practice/updown.py
@@ -0,0 +1,19 @@
+
+def up_down(stuff):
+    binary = []
+    for i in range(1, len(stuff)):
+        if stuff[i] > stuff[i-1]:
+            binary.append(1)
+        elif stuff[i] < stuff[i-1]:
+            binary.append(-1)
+        else:
+            binary.append(0)
+    return binary
+
+def compare_updown(first, second):
+    correct = 0
+    total = len(first)
+    for i in range(len(first)):
+        if first[i] == second[i]:
+            correct += 1
+    return correct, correct / total
